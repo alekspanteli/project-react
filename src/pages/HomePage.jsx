5fbaab8d6c56ad592e782ae85a3dabc1d06f3c72
Alex Panteli <alekspanteli@gmail.com>
2025-11-20 18:16:00 +0200

    Implement filtering logic in HomePage component by integrating isListingAvailable function and updating handleFilters method. The ListingFilters component now triggers filtering based on dates, guests, and search criteria, enhancing user experience.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { isListingAvailable } from '@/api/data/listings';
 import { listings as staticListings } from '@/api/data/listings';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
@@ -7,10 +8,40 @@ import { Separator } from '@/components/ui';
 
 export default function HomePage() {
   const [listings, setListings] = useState(staticListings);
+
+  const handleFilters = (filters) => {
+    const { dates, guests, search } = filters;
+
+    // Resets filters by using static listings
+    let filteredListings = staticListings;
+
+    // Handles date range
+    if (dates) {
+      filteredListings = filteredListings.filter((listing) =>
+        isListingAvailable(listing, dates),
+      );
+    }
+
+    // Handles guests
+    if (guests) {
+      filteredListings = filteredListings.filter(
+        (listing) => guests <= listing.maxGuests,
+      );
+    }
+
+    // Handles search
+    if (search) {
+      filteredListings = filteredListings.filter((listing) =>
+        listing.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    setListings(filteredListings);
+  };
   return (
     <div className='container px-6 py-4'>
       <div className='mb-4'>
-        <ListingFilters />
+        <ListingFilters onChange={handleFilters} />
         <Separator className='my-4' />
       </div>
       <ListingList listings={listings} />
