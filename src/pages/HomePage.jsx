aea3c3def5768844fa131d108be3a322cbfc18d0
Alex Panteli <alekspanteli@gmail.com>
2025-11-20 16:23:47 +0200

    Update jsconfig.json for module resolution and enhance HomePage component with state management. Changed moduleResolution to 'node', added 'lib' and 'resolveJsonModule' options, and refactored HomePage to use useState for managing listings.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
-import { listings } from '@/api/data/listings';
+import { useState } from 'react';
+
+import { listings as StaticListings } from '@/api/data/listings';
 import ListingList from '@/components/ListingList';
 
 export default function HomePage() {
+  const [listings, setListings] = useState(StaticListings);
   return (
     <div className='container px-6 py-4'>
       <ListingList listings={listings} />
